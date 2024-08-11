0b35f43e72a0e024fff8491cc5e002a02479fe6e
Rger <118863843+KeenGWatanabe@users.noreply.github.com>
2024-08-11 14:38:54 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿public class Program
 {
-    static float vat = 19f;
+    static float vat = 19f; // for scope of the class Program
 
     public static void Main() // <- method
     {
-        
+        float vat =15; // for the scope of the method
 
         Console.WriteLine("Total vat is " + vat);
     }

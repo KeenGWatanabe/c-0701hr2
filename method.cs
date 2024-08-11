d5ba4c0aa1d381972a1eec59d7c4ceda176e6e8d
KeengWatanabe <mckeenweb@gmail.com>
2024-08-11 14:23:53 +0800

    r

---
+++ method.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    static float vat = 19f;
+
+    public static void Main() // <- method
+    {
+        
+
+        Console.WriteLine("Total vat is " + vat);
+    }
+
+    public static void Calculate()
+    {
+        Console.WriteLine("Vat is  " + vat);
+    }
+}
